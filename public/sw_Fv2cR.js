bc963e21736ec0381ec128ebe8b11c5ba5bcf031
Felix Fong <moongod101@hotmail.com>
2018-05-01 18:40:58 +0800

    Started using Workbox library

--- public/sw_Fv2cR.js
+++ public/sw_Fv2cR.js
@@ -2,230 +2,93 @@
   YAY, Service worker :P
 */
 
-const version = 'v1';
-const _cacheName = `hs_cache_${version}`;
+// Import WorkBox library
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js')
 
-// List of static files that we need them later on
-const preCacheList = [
-  '/',
+// Offline Google Analytics
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js')
 
-  // The "Apple Shell" lol
-  '/index.html',
+if (workbox) {
+  workbox.googleAnalytics.initialize()
+  const version = 'v1'
+  const _cacheName = `hs_cache_${version}`
 
-  // The main application logic
-  '/static/js/bundle.js',
+  // List of static files that we need them later on
+  const preCacheList = [
+    '/',
 
-  // The offline page
-  '/offline.html',
+    // The "Apple Shell" lol
+    '/index.html',
 
-  // Web font for the offline page
-  'https://fonts.googleapis.com/css?family=Oswald',
+    // The main application logic
+    '/static/js/bundle.js',
 
-  // The HoovesSound logo
-  '/favicon.ico',
-  'https://storage.googleapis.com/hs-static/favicon.png',
+    // The offline page
+    '/offline.html',
 
-  // Emoji one sheet
-  'https://github.com/pladaria/react-emojione/blob/emojione3/assets/sprites/emojione-3.1.2-64x64.png?raw=true',
+    // Web font for the offline page
+    'https://fonts.googleapis.com/css?family=Oswald',
 
-  // Material UI defalut font
-  'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
+    // The HoovesSound logo
+    '/favicon.ico',
+    'https://storage.googleapis.com/hs-static/favicon.png',
 
-  // Firebase 4.13.0 SDK
-  'https://www.gstatic.com/firebasejs/4.13.0/firebase.js',
+    // Emoji one sheet
+    'https://github.com/pladaria/react-emojione/blob/emojione3/assets/sprites/emojione-3.1.2-64x64.png?raw=true',
 
-  // Socket.io client library
-  'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.0/socket.io.js',
-];
+    // Material UI default font
+    'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  // When the user first visit our page, this event kicks in
-  event.waitUntil(
-    caches.open(_cacheName)
-    .then(cache => {
-      cache.addAll(preCacheList);
-    })
-  );
-});
+    // Firebase 4.13.0 SDK
+    'https://www.gstatic.com/firebasejs/4.13.0/firebase.js',
 
-self.addEventListener('fetch', event => {
-  if(event.request.method === 'GET'){
+    // Socket.io client library
+    'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.0/socket.io.js',
+  ]
 
-    // When the browser try to fetch something like making an API call or getting an image from Imgur, this event kicks in
+  self.addEventListener('install', event => {
+    self.skipWaiting()
+    // When the user first visit our page, this event kicks in
+    event.waitUntil(
+      caches.open(_cacheName)
+      .then(cache => {
+        cache.addAll(preCacheList)
+      })
+    )
+  })
 
-    // Socket.io stuff
-    if(event.request.url.includes('socket.io')){
-      return event.respondWith(
-        caches.open(_cacheName)
-        .then(cache => {
-          return cache.match('https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.0/socket.io.js')
-          .then(response => {
-            if(response){
-              return response;
-            }else{
-              return fetch('https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.0/socket.io.js')
-              .then(response => {
-                cache.put(event.request, response.clone());
-                return response;
-              })
-            }
-          })
-        })
-      )
-    }
+  workbox.routing.registerRoute(
+    new RegExp(/.*\.js/i),
+    workbox.strategies.networkFirst()
+  )
 
-    if(event.request.url.includes('sockjs-node')){
-      return event.respondWith(
-        fetch(event.request)
-      )
-    }
+  workbox.routing.registerRoute(
+    new RegExp(/.*\.map/i),
+    workbox.strategies.networkFirst()
+  )
 
-    if(event.request.url.endsWith('.map')){
-      return event.respondWith(
-        fetch(event.request)
-      )
-    }
+  workbox.routing.registerRoute(
+    new RegExp(/.*sockjs-node.*/i),
+    workbox.strategies.networkFirst()
+  )
 
-    if(event.request.url.includes('google-analytics.com')){
-      return event.respondWith(
-        fetch(event.request)
-      )
-    }
+  workbox.routing.registerRoute(
+    new RegExp(/.*socket.hoovessound.*/i),
+    workbox.strategies.networkFirst()
+  )
 
-    if(event.request.url.includes('events')){
-      return event.respondWith(
-        fetch(event.request)
-      )
-    }
+  workbox.routing.registerRoute(
+    new RegExp(/.*api.hoovessound.*\/events/i),
+    workbox.strategies.networkFirst()
+  )
 
-    if(event.request.url.includes('hot-update')){
-      return event.respondWith(
-        fetch(event.request)
-      )
-    }
+  self.addEventListener('install', event => {
+    workbox.routing.registerRoute(
+      new RegExp(/.*/),
+      workbox.strategies.staleWhileRevalidate(),
+    )
+  });
 
-    // Cache first
-
-    if(event.request.url.includes('api.hoovessound') && event.request.url.includes('/image/')){
-      // HoovesSound Image API
-      // Serive cache version first
-      return event.respondWith(
-        caches.open(_cacheName).then((cache) => {
-          return cache.match(event.request).then((response) => {
-            if (response) {
-              fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-              });
-              return response;
-            }else{
-              return fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-            }
-          })
-        })
-      )
-    }
-
-    if(event.request.url.includes('fonts.googleapis.com') && event.request.method === 'GET'){
-      // HoovesSound Image API
-      // Serve cache version first
-      return event.respondWith(
-        caches.open(_cacheName).then((cache) => {
-          return cache.match(event.request).then((response) => {
-            if (response) {
-              fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-              });
-              return response;
-            }else{
-              return fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-            }
-          })
-        })
-      )
-    }
-
-    if(event.request.url.includes('stream.hoovessound') && event.request.method === 'GET'){
-      // HoovesSound Music Streaming API
-      // Serve the cache version first
-      return event.respondWith(
-        caches.open(_cacheName)
-        .then(cache => {
-          return cache.match(event.request)
-          .then(response => {
-            if (response) {
-              fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-              });
-              return response;
-            }else{
-              return fetch(event.request).then((response) => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-            }
-          })
-        })
-      )
-    }
-
-    // Network first caching method
-
-    /*
-      Flow:
-      Online: API Call -> Network fetch -> Update cache
-      Offline (with cache): API Call -> Network fetch(Fail) -> Serve cache
-      Offline (without cache): API Call -> Network fetch(Fail) -> Serve cache(Fail) -> Serve offline.html -> IDK lol
-    */
-
-    if(event.request.method === 'GET'){
-      return event.respondWith(
-        fetch(event.request)
-          .then(networkResponse => {
-            return caches.open(_cacheName)
-            .then(cache => {
-              cache.put(event.request, networkResponse.clone());
-              return networkResponse;
-            })
-          })
-          .catch(() => {
-            return caches.open(_cacheName)
-            .then(cache => {
-              return cache.match(event.request)
-              .then(response => {
-                if(response){
-                  return response;
-                }else{
-                  return cache.match('/offline.html')
-                  .then(response => {
-                    return response;
-                  })
-                }
-              })
-            })
-          })
-      );
-    }
-  }
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-    .then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if(cacheName !== _cacheName){
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    })
-  );
-});
+}else{
+  console.error('Workbox library didn\'t load, service worker don\'t execute')
+}
