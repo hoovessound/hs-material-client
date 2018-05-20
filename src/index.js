6d9a2a2245810e55b67fe484e62b26274aeb30c2
Felix Fong <moongod101@hotmail.com>
2018-05-20 18:53:08 +0800

    Check for existing open tabs of the HoovesSound application

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDom from 'react-dom';
 import Routers from './Routers';
 import './Utils/socketIoAuth';
 import './SCSS/index.scss';
+import { set } from './Utils/globalObject';
 
 const disableAnimation = localStorage.getItem('hs_disable_animation') === 'true' ? true : false;
 const firstPaintElement = document.querySelector('#first_paint');
 const statusText = document.querySelector('#status-text');
+
+// Actually, this owo thing means open tabs.... just so you know
+const openTab = localStorage.getItem('owo');
+if(openTab === 'true'){
+    console.log('You have HoovesSound open in other tab');
+    set('openTabs', true);
+}else{
+    localStorage.setItem('owo', 'true');
+    window.onbeforeunload = () => {
+        localStorage.setItem('owo', '');
+    }
+    set('openTabs', false);
+    console.log('You do not have HoovesSound open in other tab');
+}
 
 document.querySelector('#first_paint .load-bar').style.display = 'none';
 
